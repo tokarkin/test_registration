3d7619a972e9e14170e19c7d06b03dc78fbdca15
Dima Ksenchyn <42603078+tokarkin@users.noreply.github.com>
2020-02-06 22:23:02 +0200

    Updates

---
+++ precache-manifest.c8b25b68054c103074d2ab185773f556.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c21385dd743da34c06448513ce61c38d",
+    "url": "/test_registration/index.html"
+  },
+  {
+    "revision": "9cd01133e9e08701c2da",
+    "url": "/test_registration/static/css/main.7092d03f.chunk.css"
+  },
+  {
+    "revision": "f16a62841e546ec98c95",
+    "url": "/test_registration/static/js/2.19f9af89.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/test_registration/static/js/2.19f9af89.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9cd01133e9e08701c2da",
+    "url": "/test_registration/static/js/main.322a8220.chunk.js"
+  },
+  {
+    "revision": "f4321afa7d8d14e9d48e",
+    "url": "/test_registration/static/js/runtime-main.f5469fce.js"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/test_registration/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "91a48306cb76e114c2410893290c0a91",
+    "url": "/test_registration/static/media/man-laptop-v1.91a48306.png"
+  }
+]);
